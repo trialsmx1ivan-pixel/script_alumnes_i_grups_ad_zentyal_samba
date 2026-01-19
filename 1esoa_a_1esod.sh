3b361201d2174694394928f97f794a7afc58ca14
ivan <ivankiessling9@gmail.com>
2026-01-19 20:37:20 +0100

    prueba

--- 1esoa_a_1esod.sh
+++ 1esoa_a_1esod.sh
@@ -9,4 +9,23 @@ for i in {a..d}; do
     echo "1eso$i"
 done
  }
+2eso() {
+for i in {a..d}; do
+    echo "2eso$i"
+done
+ }
+3eso() {
+for i in {a..d}; do
+    echo "3eso$i"
+done
+ }
+4eso() {
+for i in {a..d}; do
+    echo "4eso$i"
+done
+ }
+
 1eso
+2eso
+3eso
+4eso
